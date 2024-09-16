f49b0c75b7a84ca91d7e98028b2cc45c5dde647a
roger <fontestz@gmail.com>
2024-09-16 15:01:00 -0300

    adicao do grant_type password

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
   name                          = each.value.client_name
   user_pool_id                  = aws_cognito_user_pool.user_pool[each.key].id
   #allowed_oauth_flows           = ["code", "implicit"]
-  allowed_oauth_flows           = ["password", "client_credentials"]
+  allowed_oauth_flows           = ["code", "implicit", "client_credentials"]
   #allowed_oauth_scopes          = ["email", "openid", "profile", "aws.cognito.signin.user.admin"]
   allowed_oauth_scopes          = ["email", "openid", "profile", "aws.cognito.signin.user.admin"]
   callback_urls                 = each.value.callback_urls
